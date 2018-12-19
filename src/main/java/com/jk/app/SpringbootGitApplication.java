3002d214cb6769b7c944ea856ade1e2bb7517c07
liuc12580 <958616386@qq.com>
2018-12-19 15:43:32 +0800

    init springboot system

---
+++ src/main/java/com/jk/app/SpringbootGitApplication.java
@@ -0,0 +1,14 @@
+package com.jk.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGitApplication.class, args);
+    }
+
+}
+
