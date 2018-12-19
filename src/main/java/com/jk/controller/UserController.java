ea712aef9505b047021356a2af284db806aad2b1
liuc12580 <958616386@qq.com>
2018-12-19 16:02:12 +0800

    添加index方法

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.jk.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("index")
+    public String index(){
+        return "hello Git";
+    }
 }
